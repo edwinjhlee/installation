0b3b0b75e5e7bf36f82720bfc4cb3fd0fc7fc714
Edwin.JH.Lee <edwin.jh.lee@gmail.com>
2016-12-05 12:15:35 +0800

    Add some adjustment.

--- docker.ubuntu-xenial.sh
+++ docker.ubuntu-xenial.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates
+sudo apt-get install apt-transport-https ca-certificates -y
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt-get update
@@ -12,6 +12,7 @@ sudo apt-get install docker-engine -y
 sudo service docker start
 sudo docker run hello-world
 
+sudo apt-get install curl -y
 curl -L https://github.com/docker/compose/releases/download/1.8.1/run.sh > /tmp/docker-compose
 sudo mv /tmp/docker-compose /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
